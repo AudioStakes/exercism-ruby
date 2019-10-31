dd7e62132d8e47d965334ecf1087e0e95fb5a76a
AudioStakes <audiostakes@gmail.com>
2019-10-31 09:06:55 +0900

    4th solution for hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,14 +1,14 @@
 class Hamming
   def self.compute(strand1, strand2)
-    raise StrandLengthError if strand1.size != strand2.size
-    strand1.each_char.zip(strand2.each_char).count do |nucleotide1, nucleotide2|
-      nucleotide1 != nucleotide2
+    raise StrandLengthError unless strand1.size == strand2.size
+    strand1.each_char.zip(strand2.each_char).count do |n1, n2|
+      n1 != n2
     end
   end
 end
 
 class StrandLengthError < ArgumentError
-  def initialize(msg = "Strand lengths must be the same length")
+  def initialize(message = "Strand lengths must be the same length")
     super
   end
 end
