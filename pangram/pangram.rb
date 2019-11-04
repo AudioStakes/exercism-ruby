d627dd7d92cf4deccff47c917585d891f3815198
AudioStakes <audiostakes@gmail.com>
2019-11-05 08:56:40 +0900

    2nd solution for pangram

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,7 +1,5 @@
 class Pangram
   def self.pangram?(sentence)
-    every_letter = ("a".."z").to_a
-    sentence.downcase.chars.each { |n| every_letter.delete(n) }
-    every_letter.size == 0
+    ([*'a'..'z'] - sentence.downcase.chars).empty?
   end
 end
