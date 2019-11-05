e8e33e7091e220210acf8d6345d216de9e2912f7
AudioStakes <audiostakes@gmail.com>
2019-11-05 22:16:12 +0900

    2nd solution for armstrong-numbers

--- armstrong-numbers/armstrong_numbers.rb
+++ armstrong-numbers/armstrong_numbers.rb
@@ -1,10 +1,5 @@
 class ArmstrongNumbers
   def self.include?(number)
-    @digits = number.to_s.chars
-    power = 0
-    @digits.each do |d|
-      power += d.to_i ** @digits.size
-    end
-    number == power
+    number.digits.sum { |d| d ** number.digits.size } == number
   end
 end
