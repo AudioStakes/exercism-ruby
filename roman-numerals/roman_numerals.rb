00de2aa847677d7aeccc190b5194a6788774cff2
AudioStakes <audiostakes@gmail.com>
2019-11-08 23:09:04 +0900

    2nd solution for roman-numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,20 +1,24 @@
 class Integer
-  ROMAN = {
-    '1000' => 'M','900' => 'CM',
-    '500' => 'D', '400' => 'CD',
-    '100' => 'C', '90' => 'XC',
-    '50' => 'L', '40' => 'XL',
-    '10' => 'X', '9' => 'IX',
-    '5' => 'V', '4' => 'IV',
-    '1' => 'I'
+  ROMAN_HASH = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
   }
   def to_roman
-    roman = ""
-    remain = self
-    ROMAN.each do |num, value|
-      roma, remain = remain.divmod(num.to_i)
-      roman << value * roma
+    remaining = self
+    ROMAN_HASH.inject('') do |result, (arabic, roman)|
+      n, remaining = remaining.divmod(arabic.to_i)
+      result << roman * n
     end
-    roman
   end
 end
