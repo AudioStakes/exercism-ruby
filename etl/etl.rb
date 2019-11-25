7af7c3ba274bc89d96d57643d45d61f84d34d493
AudioStakes <audiostakes@gmail.com>
2019-11-25 18:48:15 +0900

    2nd for etl

--- etl/etl.rb
+++ etl/etl.rb
@@ -1,37 +1,8 @@
 class ETL
-  OLD_LETTERS_PER_SCORE = {
-    "A" => 1,
-    "B" => 3,
-    "C" => 3,
-    "D" => 2,
-    "E" => 1,
-    "F" => 4,
-    "G" => 2,
-    "H" => 4,
-    "I" => 1,
-    "J" => 8,
-    "K" => 5,
-    "L" => 1,
-    "M" => 3,
-    "N" => 1,
-    "O" => 1,
-    "P" => 3,
-    "Q" => 10,
-    "R" => 1,
-    "S" => 1,
-    "T" => 1,
-    "U" => 1,
-    "V" => 4,
-    "W" => 4,
-    "X" => 8,
-    "Y" => 4,
-    "Z" => 10
-  }
-
   def self.transform(old)
-    old.inject({}) do |new_format, (number, letters)|
+    old.inject({}) do |new_format, (score, letters)|
       letters.inject(new_format) do |new_format, value|
-        new_format[value.downcase] = number; new_format
+        new_format[value.downcase] = score; new_format
       end
     end
   end
