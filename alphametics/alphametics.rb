bad35e6106f14f954c6776c1c0a2f128e749d281
AudioStakes <audiostakes@gmail.com>
2019-12-25 22:24:14 +0900

    2nd solution for alphametrics

--- alphametics/alphametics.rb
+++ alphametics/alphametics.rb
@@ -1,17 +1,10 @@
-require 'set'
 class Alphametics
   def self.solve(puzzle)
-    puzzle = puzzle.upcase
-    words = puzzle.scan /[A-Z]+/
-    chars = Set.new words.join.each_char
-    abort 'Too many letters' if chars.size > 10
-    first_chars = Set.new words.select {|w| w.size > 1 }.map {|w| w[0] }
-    n = first_chars.size
-    sorted_chars = first_chars.to_a.join + (chars - first_chars).to_a.join
-    %w[0 1 2 3 4 5 6 7 8 9].permutation(chars.size).each do |guess|
-      next if guess[0, n].member? '0'
-      expr = puzzle.tr sorted_chars, guess.join
-      return sorted_chars.chars.zip(guess.map(&:to_i)).to_h if eval expr
+    uniq_chars = puzzle.scan(/\w/).uniq.sort
+    (0..9).to_a.permutation(uniq_chars.size).each do |digits|
+      replace = puzzle.tr(uniq_chars.join, digits.join)
+      next if replace.scan(/\d+/).any? { |n| n[0] == '0' }
+      return uniq_chars.zip(digits).to_h if eval(replace)
     end
     return Hash.new
   end
