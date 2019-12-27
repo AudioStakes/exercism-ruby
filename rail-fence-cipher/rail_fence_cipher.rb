8575c1577b6e276096b3b9287bb33ad25982854c
AudioStakes <audiostakes@gmail.com>
2019-12-27 23:59:12 +0900

    rail_fence_cipher while solving 2

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -1,11 +1,12 @@
 class RailFenceCipher
   def self.encode(message, stages)
-    skip_num = (stages - 1) * stages / 2
+    multiple = ((stages - 1) * stages / 2 ) + 1
     array = []
     (1..stages).each do |stage|
       message.chars.each_with_index do |char, i|
-        array << char if (i - stage - 1) % (skip_num + 1) == 0
+        array << char if (i + stage - 1) % multiple == 0
       end
     end
+    array.join
   end
 end
