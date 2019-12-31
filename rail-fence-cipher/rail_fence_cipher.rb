9714a5e52127cbc2319b2a34dd5c180e28a77df9
AudioStakes <audiostakes@gmail.com>
2019-12-31 18:38:32 +0900

    2nd solution for rail_fence_cipher

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -1,51 +1,23 @@
 class RailFenceCipher
-  def self.encode(message, stages)
-    return message if stages == 1
-    zig_zag = stages * 2 - 2
-    middle_zig_zag = zig_zag / 2
-    array = []
-    message.chars.each_with_index do |char, i|
-      if i % zig_zag <= middle_zig_zag
-        stage = i % zig_zag
-      else
-        stage = (i % zig_zag - (middle_zig_zag * 2)).abs
-      end
-      array << [char, stage]
-    end
-    zig_zag_array = []
-    (0..stages).each do |stage|
-      array.each do |char, num|
-        zig_zag_array << char if num == stage
-      end
-    end
-    zig_zag_array.join
+  def self.decode(ciphertext, stages)
+    all_of_zigzag(stages, ciphertext.length)
+    .sort
+    .zip(ciphertext.chars)
+    .sort_by { |a| a[0][1] }
+    .map { |a| a[1] }
+    .join
   end
 
-  def self.decode(message, stages)
-    return message if stages == 1
-    zig_zag = stages * 2 - 2
-    middle_zig_zag = zig_zag / 2 + 1
-    multiple, amari = message.size.divmod(zig_zag)
-    hash = {}
-    @message_chars = message.chars
-    (1..stages).each do |stage|
-      if stage == 1 || stage == stages
-        num = multiple
-      else
-        num = multiple * 2
-      end
-      if stage <= amari
-        num += 1
-      end
-      hash[stage] = @message_chars.slice!(0, num)
-    end
-    array = []
-    while hash[1].size != 0 do
-      (1..zig_zag).each do |stage|
-        stage = middle_zig_zag * 2 - stage if stage > middle_zig_zag
-        array << hash[stage].shift
-      end
-    end
-    array.join
+  def self.encode(plaintext, stages)
+    all_of_zigzag(stages, plaintext.length)
+      .zip(plaintext.chars)
+      .sort
+      .map { |a| a[1] }
+      .join
+  end
+
+  def self.all_of_zigzag(stages, size)
+    one_of_zigzag = (0..stages - 1).to_a + (1..stages - 2).to_a.reverse
+    one_of_zigzag.cycle.first(size).zip(0..size)
   end
 end
