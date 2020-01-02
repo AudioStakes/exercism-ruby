7a345b7ccee654e6135b97cba94af32a04325a39
AudioStakes <audiostakes@gmail.com>
2020-01-02 22:49:08 +0900

    2nd solution for nucleotide-count

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -1,18 +1,20 @@
 class Nucleotide
+  NUCLEOTIDE_TYPES = "ATCG"
+
   def initialize(dna)
     @dna = dna
   end
 
   def self.from_dna(dna)
-    raise ArgumentError unless dna.chars.all? { |n| n == 'A' || n == 'T' || n == 'C' || n == 'G' }
-    Nucleotide.new(dna)
+    raise ArgumentError if /[^#{NUCLEOTIDE_TYPES}]/ =~ dna
+    new(dna)
   end
 
-  def count(nucleotide)
-    @dna.chars.count { |n| n == nucleotide}
+  def count(n)
+    @dna.count(n)
   end
 
   def histogram
-    { 'A' => count('A'), 'T' => count('T'), 'C' => count('C'), 'G' => count('G') }
+    NUCLEOTIDE_TYPES.chars.map { |n| [n, count(n)] }.to_h
   end
 end
